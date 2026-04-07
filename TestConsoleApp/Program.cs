499145173cc00d3fcf23f92febf88784f9bfb154
Trizha Bacani <tbacani@my.centennialcollege.ca>
2026-04-07 13:56:39 -0400

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using StringExtensionsLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string test1 = "Hello";
+        string test2 = "hello";
+
+        Console.WriteLine($"\"{test1}\" starts with uppercase: {test1.StartsWithUpper()}");
+        Console.WriteLine($"\"{test2}\" starts with uppercase: {test2.StartsWithUpper()}");
+    }
+}
